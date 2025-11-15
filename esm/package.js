4a879626348b7c4a7ababbf1ef0913222854f2cb
rcheiko <cheikororo69005@hotmail.fr>
2025-11-15 01:30:14 +0100

    fix publish on npm

---
+++ esm/package.js
@@ -0,0 +1,11 @@
+export default {
+    "name": "@swade-app/lucid",
+    "version": "1.0.0",
+    "license": "MIT",
+    "author": "Alessandro Konrad",
+    "description": "Lucid is a library, which allows you to create Cardano transactions and off-chain code for your Plutus contracts in JavaScript, Deno and Node.js.",
+    "repository": "https://github.com/rcheiko/lucid-cardano",
+    "dependencies": {
+        "@types/node": "^24.10.1"
+    }
+};
---
+++ types/package.d.ts
@@ -0,0 +1,12 @@
+declare namespace _default {
+    const name: string;
+    const version: string;
+    const license: string;
+    const author: string;
+    const description: string;
+    const repository: string;
+    const dependencies: {
+        "@types/node": string;
+    };
+}
+export default _default;
